71502de6f870bbac396fe06f3c7f796b435965cc
RytisMark <rytismarkeviciuss@gmail.com>
2023-02-12 15:41:07 +0200

    first 7 excercises done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...whatToRemove) {
+    for (const val of whatToRemove){
+        var i = 0;
+        while (i < arr.length) {
+            if (arr[i] === val) {
+                arr.splice(i, 1);
+                } else {
+                ++i;
+                }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(numFrom, numTo) {
+    if (!(Number.isInteger(numFrom) && Number.isInteger(numTo)) || (numFrom < 0 || numTo < 0)) return "ERROR";
+    if (numFrom > numTo) {
+        let temp = numTo;
+        numTo = numFrom;
+        numFrom = temp;
+    }
+    
+    let sum = 0;
+    while(numFrom <= numTo) {
+        sum += numFrom;
+        numFrom++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
